aaf2d2de0b9e9bf3b91a3fe8ce5e25648305ef68
Emmanuel Koumandakis <manykoum@msn.com>
2017-03-29 03:42:44 -0400

    Fixed it; now it works but haven't tested edge cases yet

--- bpred.cpp
+++ bpred.cpp
@@ -17,14 +17,14 @@ KNOB<UINT32> KnobK(KNOB_MODE_WRITEONCE, "pintool", "k", "0", "Branch PC bits to 
 // their usage, as needed.  However the final output format (see Fini, 
 // below) should be unchanged.
 uint total_bits = 0;
-float accuracy = 0;
+float accuracy = 0.0;
 uint n = 0;
 uint m = 0;
 uint k = 0;
 uint total_branches = 0;
 uint total_taken = 0;
 uint total_fallthru = 0;
-uint total_mispredicts = 0;
+uint total_predicts = 0;
 uint *preds;
 uint nlimit = 0;
 uint threshold = 0;
@@ -35,15 +35,15 @@ unsigned short addr_mask = 0;
 void updateGHR(bool taken) {
     GHR = GHR << 1;
     if (taken)
-        GHR++;
+        GHR|1;
 }
 
 bool predict_taken(unsigned short idx) {
     uint counter = preds[idx];
 
-    if (counter > threshold) {
-        return true;
-    return false;
+    if (counter < threshold) 
+        return false;
+    return true;
 }
 
 void updateCounter(unsigned short idx, bool taken) {
@@ -52,7 +52,7 @@ void updateCounter(unsigned short idx, bool taken) {
             preds[idx] = preds[idx] + 1;
     } else {
         if (preds[idx] > 0)
-            preds[idx] = pres[idx] - 1;
+            preds[idx] = preds[idx] - 1;
     }
 }
 // Invoked once per dynamic branch instruction
@@ -60,18 +60,18 @@ void updateCounter(unsigned short idx, bool taken) {
 // taken: Non zero if a branch is taken
 VOID DoBranch(ADDRINT pc, BOOL taken) {
     total_branches++;
-    unsigned short idx_addr = pc && addr_mask;
-    unsigned short idx = ((GHR&&GHR_mask)<<k)+idx_addr;
+    unsigned short idx_addr = pc & addr_mask;
+    unsigned short idx = ((GHR&GHR_mask)<<k)|idx_addr;
     bool pred_taken = predict_taken(idx);
 
     if (taken) {
         total_taken++;
-        if (!pred_taken)
-            total_mispredicts++;
+        if (pred_taken)
+            total_predicts++;
     } else {
         total_fallthru++;
-        if (pred_taken)
-            total_mispredicts++;
+        if (!pred_taken)
+            total_predicts++;
     }
     // for efficiency updateCounter() and predict_taken() can be merged
     updateCounter(idx, taken);     
@@ -108,7 +108,10 @@ VOID Fini(int, VOID * v) {
     std::ofstream out;
     filename =  KnobOutputFile.Value();
 
+    accuracy = (float)total_predicts / (float)total_branches;
+    
     out.open(filename.c_str());
+    out << "total_predicts " << total_predicts << endl;
     out << "m: " << KnobM.Value() << endl;
     out << "n: " << KnobN.Value() << endl;
     out << "k: " << KnobK.Value() << endl;
@@ -116,7 +119,7 @@ VOID Fini(int, VOID * v) {
     out << "total_taken: " << total_taken << endl;
     out << "total_fallthru: " << total_fallthru << endl;
     out << "total_bits: " << total_bits << endl;
-    out << "accuracy: " << setprecision(3) << accuracy << endl;
+    out << "accuracy: " << accuracy << setprecision(3) << endl;
 
     out.close();
 }
@@ -135,19 +138,17 @@ int main(int argc, CHAR *argv[]) {
     /* Space occupied by the Correlating Branch predictor */
     uint budget = 1 << (m + k);
     // could make it smaller since n is definitely less than 64 bits long
-    preds = calloc(budget, sizeof(uint));
+    preds = (uint *) calloc(budget, sizeof(uint));
     
     nlimit = 1 << n;
     threshold = nlimit/2;
     
     GHR_mask = (1<<m)-1;
     addr_mask = (1<<k)-1;
+    
+    total_bits = budget*n;
 
     IMG_AddInstrumentFunction(Image, 0);
-
-    total_bits = budget*n;
-    accuracy = 1 - ((float) total_mispredicts/total_branches);
-    
     PIN_AddFiniFunction(Fini, 0);
 
     PIN_StartProgram();
